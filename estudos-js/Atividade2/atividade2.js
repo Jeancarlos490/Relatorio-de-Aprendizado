5489d916e94f2debf3694bf933845755942cbfd3
Jeancarlos490 <Jeanc8752@gmail.com>
2025-12-22 12:53:27 -0300

    Atualização: adição do botão para remover o ultimo elemento da lista

--- estudos-js/Atividade2/atividade2.js
+++ estudos-js/Atividade2/atividade2.js
@@ -7,15 +7,27 @@ function AtualizarLista() {
     let valor = Number(document.getElementById("gasto-valor").value);
     const res = document.getElementById("resultado");
     const attlist = document.createElement("li");
-
-
-    if (desc !== "" && !isNaN(valor)) {
+    
+    if (desc !== "" && valor > 0 && !isNaN(valor)) {
         listanome.push(desc);
         listavalor.push(valor);
         const arraynome = listanome.length - 1;
-        const arrayvalor = listanome.length - 1;
-        attlist.innerHTML = `${listanome[arraynome]}: ${listavalor[arrayvalor]}`;
+        const arrayvalor = listavalor.length - 1;
+        attlist.innerText = `${listanome[arraynome]}: ${listavalor[arrayvalor]}`;
         res.appendChild(attlist);
+    } else {
+        alert("preencha todos os campos")
     }
+}
 
+function removerultimo(){
+    const res = document.getElementById("resultado")
+
+    if (res.lastElementChild) {
+        res.lastElementChild.remove();
+        listanome.pop();
+        listavalor.pop();
+    }else {
+        alert("lista vazia")
+    }
 }
