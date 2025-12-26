3b97501a9c02a3c049ed67fa2d39f42d6d1efd4a
Jeancarlos490 <Jeanc8752@gmail.com>
2025-12-26 20:24:35 -0300

    Primeira tentativa de somar os valores da lista (tentativa falha)

--- estudos-js/Atividade2/atividade2.js
+++ estudos-js/Atividade2/atividade2.js
@@ -7,27 +7,42 @@ function AtualizarLista() {
     let valor = Number(document.getElementById("gasto-valor").value);
     const res = document.getElementById("resultado");
     const attlist = document.createElement("li");
-    
+
     if (desc !== "" && valor > 0 && !isNaN(valor)) {
         listanome.push(desc);
         listavalor.push(valor);
         const arraynome = listanome.length - 1;
-        const arrayvalor = listavalor.length - 1;
+        let arrayvalor = Number(listavalor.length - 1);
         attlist.innerText = `${listanome[arraynome]}: ${listavalor[arrayvalor]}`;
         res.appendChild(attlist);
+        TotalValor()
     } else {
         alert("preencha todos os campos")
     }
+
+    function TotalValor() {
+        let primeirovalor = listavalor.at(-2)
+        let ultimovalor = listavalor.at(-1)
+        let valortotal = primeirovalor += ultimovalor
+        const resultado = document.getElementById("Valortotal")
+
+        if(listavalor.length <= 1){
+            resultado.innerHTML = ultimovalor
+        }
+    }
 }
 
-function removerultimo(){
+function Removerultimo() {
     const res = document.getElementById("resultado")
+    const valortot = document.getElementById("Valortotal")
 
     if (res.lastElementChild) {
         res.lastElementChild.remove();
         listanome.pop();
         listavalor.pop();
-    }else {
+        let valoresgerais = listavalor.at(-0) - listavalor.at(-1)
+        valortot.innerText = valoresgerais;
+    } else {
         alert("lista vazia")
     }
 }
